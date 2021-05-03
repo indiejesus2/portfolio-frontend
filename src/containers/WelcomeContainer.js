e78a1b779eea5b682157a19169e89d827f1cdc2c
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-05-02 20:46:34 -0400

    WIP: Initial Welcome Page is set up.

---
+++ src/containers/WelcomeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class WelcomeContainer extends Component {
+
+    render() {
+        return (
+            <div className="welcome">
+                <h1>Hello! I'm Dan Ste. Marie. Welcome to my Homepage!</h1>
+            </div>
+        )
+    }
+}
+
+export default WelcomeContainer
