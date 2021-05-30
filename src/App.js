bf4f681252c744f4ccfb13d4302d871d519a13ce
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-05-30 17:08:08 -0400

    WIP: Figured out how to add images. Also added Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React,{ Component } from 'react'
 import PagesContainer from './containers/PagesContainer';
 import NavBar from './components/NavBar'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+// import 'bootstrap/dist/css/boostrap.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <NavBar />
+          <div>
+            <Jumbotron>
+              <h1>Daniel Ste. Marie</h1>
+              <NavBar />
+            </Jumbotron>
+          </div>
           <PagesContainer />
       </div>
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ class Projects extends Component {
             <div>
                 <Link target={"{}"} to={{ pathname: "https://www.github.com/indiejesus2/counting-cookies"}}>Counting Cookies</Link>
                 <Link to={{ pathname: "https://drive.google.com/file/d/1PYk54rWBVbEonz_Nslf5JXcbIvTyGYZr/view" }} target={"{}"}>
-                    <img src="C:\Users\dstem\dev\flatiron\labs\portfolio\public\Frisbee_Golf_Course.png" alt="Demo" width="42" height="42" />
+                    <img src='images/Frisbee_Golf_Course.png' alt="Demo" width="150" height="150" />
                 </Link>
             </div>
         )
