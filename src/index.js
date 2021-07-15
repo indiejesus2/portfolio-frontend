d997f828a76a134f69762e61c9ce732202aeb5bb
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-07-14 23:43:51 -0400

    WIP:Adjusted index page to handle routing.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Frontend/src/App';
-import reportWebVitals from './Frontend/src/reportWebVitals';
-import {HashRouter as Router} from 'react-router-dom'
+import App from './src/App';
+import {BrowserRouter as Router} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
     <Router>
       <App />
-    </Router>
-  </React.StrictMode>,
+    </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
