54a8d31feb190f2ca7a10dd918e886bc798e114b
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-08-29 23:13:02 -0400

    WIP: Updated the contact page to include links.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,12 +8,25 @@ class Contact extends Component {
                 <h1>CONTACT ME</h1>
                 <p>I am available for hire and open to any ideas of cooperation.</p>
                 <div className="contact">
+                    <h1>Links</h1>
                     <table>
                         <tr>
-                            <td>
+                            {/* <td>
                                 Email:   
-                            </td>
-                            <td><a href="mailto:dste.mariejr@gmail.com">Daniel Ste. Marie</a></td>
+                            </td> */}
+                            <td><a href="mailto:dste.mariejr@gmail.com">Email</a></td>
+                        </tr>
+                        <tr>
+                            {/* <td>
+                                LinkedIn:   
+                            </td> */}
+                            <td><a href="https://www.linkedin.com/in/daniel-ste-marie-jr-0b419b61/"> LinkedIn</a></td>
+                        </tr>
+                        <tr>
+                            {/* <td>
+                                Github:   
+                            </td> */}
+                            <td><a href="https://www.github.com/indiejesus2">Github</a></td>
                         </tr>
                     </table>
                 </div>
